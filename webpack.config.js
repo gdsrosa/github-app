0b1f46195616056ebdf50c8e40d907d9257ec4d0
gdsrosa <gabrieldsrosa@gmail.com>
2018-09-13 00:02:18 -0300

    Remove standard config

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,33 @@
-'use strict'
+"use strict";
 
-const path = require('path')
-const webpack = require('webpack')
-const validate = require('webpack-validator')
+const path = require("path");
+const webpack = require("webpack");
+const validate = require("webpack-validator");
 
 module.exports = validate({
-  devtool: 'source-map',
+  devtool: "source-map",
   entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    path.join(__dirname, 'src', 'index'),
+    "react-hot-loader/patch",
+    "webpack-dev-server/client?http://localhost:3000",
+    "webpack/hot/only-dev-server",
+    path.join(__dirname, "src", "index")
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/dist/'
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist/"
   },
 
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 
   module: {
-    preLoaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      include: /src/,
-      loader: 'standard'
-    }],
-    loaders: [{
-    test: /\.js$/,
-    exclude: /node_modules/,
-    include: /src/,
-    loader: 'babel'
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: /src/,
+        loader: "babel"
+      }
+    ]
   }
-})
+});
