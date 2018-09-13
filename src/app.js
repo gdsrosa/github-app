17e4877b0279d75d160e1a65deb386716b56401f
gdsrosa <gabrieldsrosa@gmail.com>
2018-09-12 23:49:31 -0300

    Refactor to spread operator

--- src/app.js
+++ src/app.js
@@ -1,84 +1,84 @@
-'use strict'
-import React, { Component } from 'react'
-import AppContent from './components/app-content'
-import ajax from '@fdaciuk/ajax'
+"use strict";
+import React, { Component } from "react";
+import AppContent from "./components/app-content";
+import ajax from "@fdaciuk/ajax";
 
 class App extends Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
     this.state = {
       userInfo: null,
       repos: [],
       starred: [],
       isFetching: false
-    }
+    };
 
-    this.handleSearch = this.handleSearch.bind(this)
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
-  getGithubApiUrl (username, type) {
-    const interalUser = username ? `${username}` : ''
-    const internalType = type ? `/${type}` : ''
-    return `https://api.github.com/users/${interalUser}${internalType}`
+  getGithubApiUrl(username, type) {
+    const interalUser = username ? `${username}` : "";
+    const internalType = type ? `/${type}` : "";
+    return `https://api.github.com/users/${interalUser}${internalType}`;
   }
 
-  handleSearch (e) {
-    const value = e.target.value
-    const keyCode = e.which || e.keyCode
-    const ENTER = 13
-    const target = e.target
+  handleSearch(e) {
+    const value = e.target.value;
+    const keyCode = e.which || e.keyCode;
+    const ENTER = 13;
+    const target = e.target;
 
     if (keyCode === ENTER) {
-      this.setState({ isFetching: true })
-      ajax().get(this.getGithubApiUrl(value)).then((result) => {
-        this.setState({
-          userInfo: {
-            username: result.name,
-            login: result.login,
-            photo: result.avatar_url,
-            repos: result.public_repos,
-            followers: result.followers,
-            following: result.following
-          },
-          repos: [],
-          starred: []
+      this.setState({ isFetching: true });
+      ajax()
+        .get(this.getGithubApiUrl(value))
+        .then(result => {
+          this.setState({
+            userInfo: {
+              username: result.name,
+              login: result.login,
+              photo: result.avatar_url,
+              repos: result.public_repos,
+              followers: result.followers,
+              following: result.following
+            },
+            repos: [],
+            starred: []
+          });
         })
-      })
-      .always(() => {
-        target.disabled = true
-        this.setState({ isFetching: false })
-      })
+        .always(() => {
+          target.disabled = true;
+          this.setState({ isFetching: false });
+        });
     }
   }
 
-  getRepos (type) {
-    return (e) => {
-      const username = this.state.userInfo.login
-      ajax().get(this.getGithubApiUrl(username, type))
-        .then((result) => {
+  getRepos(type) {
+    return e => {
+      const username = this.state.userInfo.login;
+      ajax()
+        .get(this.getGithubApiUrl(username, type))
+        .then(result => {
           this.setState({
-            [type]: result.map((repo) => ({
+            [type]: result.map(repo => ({
               name: repo.name,
               link: repo.html_url
             }))
-          })
-        })
-    }
+          });
+        });
+    };
   }
 
-  render () {
+  render() {
     return (
       <AppContent
-        userInfo={this.state.userInfo}
-        repos={this.state.repos}
-        starred={this.state.starred}
-        isFetching={this.state.isFetching}
+        {...this.state}
         handleSearch={this.handleSearch}
-        getRepos={this.getRepos('repos')}
-        getStarred={this.getRepos('starred')}
+        getRepos={this.getRepos("repos")}
+        getStarred={this.getRepos("starred")}
       />
-    )
+    );
   }
 }
 
-export default App
+export default App;
